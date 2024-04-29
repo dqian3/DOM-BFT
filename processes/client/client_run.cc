cef5b14dd4e78fec766534b4918fd691c55a5cb9
ManasVegi <manasvegi@gmail.com>
2024-04-29 23:10:31 +0000

    Call new constructor to use unified config

--- processes/client/client_run.cc
+++ processes/client/client_run.cc
@@ -1,11 +1,14 @@
 #include "client.h"
 DEFINE_string(config, "configs/client.yaml", "The config file for the client");
+//define a new string called clientId and read it from command line 
+//use that client id and pass it as a constructor arg.
+DEFINE_uint32(client_id, 0, "The client id.");
 dombft::Client* client = NULL;
 int main(int argc, char* argv[]) {
     gflags::ParseCommandLineFlags(&argc, &argv, true);
     google::InitGoogleLogging(argv[0]);
     FLAGS_logtostderr = 1;
-    client = new dombft::Client(FLAGS_config);
+    client = new dombft::Client(FLAGS_client_id);
     client->Run();
     delete client;
 }
