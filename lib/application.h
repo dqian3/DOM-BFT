59b2767aa8cbe8bc7ca299e596d192434b068593
PeterYaoNYU <yy4108@nyu.edu>
2024-08-06 11:38:35 -0400

    modify the interface at the application.h

--- lib/application.h
+++ lib/application.h
@@ -22,6 +22,26 @@ public:
     // virtual uint32_t takeSnapshot() = 0;
     // virtual bool restoreSnapshot() = 0;
 
+    virtual std::string execute(const std::string &serialized_request) = 0;
+
+    virtual bool commit(uint32_t commit_idx) = 0;
+
+    // TODO I am uncertain whether to use a unique pointer here. 
+    virtual std::unique_ptr<byte[]> getDigest(uint32_t digest_idx) = 0;
+
+    virtual std::unique_ptr<byte[]> takeSnapshot() = 0;
+
+    // resetting the application state to the committeed state.
+    virtual bool abort() = 0;
+
+};
+
+class AppTrafficGen {
+public:
+    virtual ~AppTrafficGen() = default;
+
+    // Virtual function to generate app traffic
+    virtual void* generateAppTraffic() = 0;
 };
 
 #endif
