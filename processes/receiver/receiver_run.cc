b8222ce91093b44b2a609310b2630e43a3c25aef
Hoooao <93057312+Hoooao@users.noreply.github.com>
2024-08-28 06:24:43 +0800

    [WIP] Reorder Analysis (#36)
    * Reorder Analysis init
    
    * Add frequency control
    
    * Structured logging needed
    
    * init fin
    
    * format
    
    * refactor
    
    * use timer instead of thread sleep
    
    * add exponential dist option
    
    * fixes
    
    * fixes to clis and scripts
    
    * cleanup receiver logs
    
    * add reordering analysis notebook
    
    * add new metrics
    
    * add task
    
    * small tweaks
    
    * add ignore deadlines option
    
    * add params to tasks
    
    * add filename
    
    * measure rtt and new prod deployment
    
    * add LIS
    
    * add sampling of measuremennts
    
    * add script for proxy processing
    
    ---------
    
    Co-authored-by: Daniel Qian <daniel.qian@nyu.edu>

--- processes/receiver/receiver_run.cc
+++ processes/receiver/receiver_run.cc
@@ -3,6 +3,10 @@
 
 DEFINE_string(config, "configs/config.yaml", "The config file for the receiver");
 
+// for reorder analysis
+DEFINE_bool(skipForwarding, false, "Whether to skip forwarding (for reordering experiments).");
+DEFINE_bool(ignoreDeadlines, false, "Whether to ignore deadlines (for reordering experiments).");
+
 dombft::Receiver *receiver = NULL;
 DEFINE_uint32(receiverId, 0, "The receiver id.");
 int main(int argc, char *argv[])
@@ -15,6 +19,6 @@ int main(int argc, char *argv[])
     ProcessConfig config;
     config.parseConfig(FLAGS_config);
 
-    dombft::Receiver receiver(config, FLAGS_receiverId);
+    dombft::Receiver receiver(config, FLAGS_receiverId, FLAGS_skipForwarding, FLAGS_ignoreDeadlines);
     receiver.run();
 }
