4eefa9fb290e2123d797cae925f5e8fda11d09e4
Hoooao <hao021014@163.com>
2024-10-06 15:51:51 -0400

    test

--- processes/replica/replica.cc
+++ processes/replica/replica.cc
@@ -296,8 +296,6 @@ void Replica::handleMessage(MessageHeader *hdr, byte *body, Address *sender)
         if (fallbackTriggerMsg.has_proof()) {
             // TODO verify proof
             LOG(INFO) << "Fallback trigger has a proof, starting fallback!";
-
-            broadcastToReplicas(fallbackTriggerMsg, FALLBACK_TRIGGER);
             startFallback();
         } else {
             endpoint_->RegisterTimer(fallbackStartTimer_.get());
