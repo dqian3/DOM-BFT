e9e1bb33f769966c519ba7782e90034d45c2a811
Daniel Qian <daniel.qian@nyu.edu>
2024-10-10 15:44:11 -0400

    revert receiver verification usage

--- processes/receiver/receiver.h
+++ processes/receiver/receiver.h
@@ -8,15 +8,22 @@
 #include "lib/utils.h"
 #include "proto/dombft_proto.pb.h"
 
-#include "lib/verification_manager.h"
-
 #include <fstream>
 #include <iostream>
-#include <thread>
 #include <mutex>
-#include <vector>
+#include <thread>
+#include <queue>
+#include <condition_variable>
 
 #include <yaml-cpp/yaml.h>
+
+struct Request {
+    dombft::proto::DOMRequest request;
+    uint64_t deadline;
+    uint32_t clientId;
+
+    bool verified = false;
+};
 
 namespace dombft {
 class Receiver {
@@ -26,70 +33,48 @@ private:
     /** The receiver uses this endpoint to receive requests from proxies and reply with OWD measurements*/
     std::unique_ptr<Endpoint> endpoint_;
 
-    std::unique_ptr<Endpoint> forwardEp_;
-
-    std::map<std::string, std::thread> threads_;
-
-    // ConcurrentQueue<dombft::proto::DOMRequest> requestQueue_;
-
-    void LaunchThreads();
-
-    void ReceiveTd();
-    void ForwardTd();
-
-    /** The handler objects for our endpoint library */
-    // TODO shared pointer for endpoint and timer??
+    /** Timer to trigger checking deadlineQueue to forward messages */
     std::unique_ptr<Timer> fwdTimer_;
 
-    std::unique_ptr<Timer> heartbeatTimer_;
+    // Map for requests to be forwarded in deadline order
+    std::mutex deadlineQueueMtx_;
+    std::map<std::pair<uint64_t, uint32_t>, std::shared_ptr<Request>> deadlineQueue_;
+    uint64_t lastFwdDeadline;
 
-    std::unique_ptr<Timer> queueTimer_;
+    // Queue for worker threads to trigger verify tasks through
+    // ConcurrentQueue<std::shared_ptr<Request>> verifyQueue_;
 
+    // Queue for worker threads to trigger verify tasks through
+    std::mutex verifyQueueMtx_;
+    std::condition_variable verifyQueueCondVar_;
+    std::queue<std::shared_ptr<Request>> verifyQueue_;
 
-    struct RequestInfo {
-        std::shared_ptr<dombft::proto::DOMRequest> request;
-        bool verified;
-        uint64_t deadline;
-    };
-
-    // TODO storing these protobuf objects like this might not be great performance wise
-    // couldn't find much about this.
-    std::map<std::pair<uint64_t, uint32_t>, RequestInfo> deadlineQueue_;
-    std::mutex deadlineQueueMutex_;
-    std::vector<dombft::proto::DOMRequest> lateMessages;
+    std::vector<std::thread> verifyThds_;
 
     /** The actual message / timeout handlers */
     void receiveRequest(MessageHeader *msgHdr, byte *msgBuffer, Address *sender);
 
-    void forwardRequest(const dombft::proto::DOMRequest &request);
     void checkDeadlines();
+    void forwardRequest(const dombft::proto::DOMRequest &request);
 
-    // void addToDeadlineQueue();
+    void verifyWorker();
 
     uint32_t receiverId_;
     uint32_t proxyMeasurementPort_;
     uint32_t numReceivers_;
     Address replicaAddr_;
 
-    // Skip forwarding, for running experiemnts.
+    // Turn off various receiver behaviors, for running micro-experiments between proxy and receiver
     bool skipForwarding_;
     bool ignoreDeadlines_;
+    bool skipVerify_;
 
     bool running_;
 
-
-    uint32_t f_;
-    size_t threadPoolSize_;
-
-
-    VerificationManager verificationManager_;
-
-
 public:
     Receiver(const ProcessConfig &config, uint32_t receiverId, bool skipForwarding = false,
-             bool ignoreDeadlines_ = false);
+             bool ignoreDeadlines = false, bool skipVerify = false);
     ~Receiver();
-    void run();
 };
 
 }   // namespace dombft
