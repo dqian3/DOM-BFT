eace166ae85371c6960d8556a1bd8fd34392c38e
Hoooao <hao021014@163.com>
2024-10-24 00:23:46 -0400

    recover record check/update in commit and fb

--- lib/fallback_utils.h
+++ lib/fallback_utils.h
@@ -9,15 +9,16 @@
 // Struct that references the checkpoint and order of entries to use within
 // the given FallbackProposal message
 struct LogSuffix {
-    // For logign purposes
+    // For loging purposes
     uint32_t replicaId;
     uint32_t instance;
 
     const dombft::proto::LogCheckpoint *checkpoint;
     std::vector<const dombft::proto::LogEntry *> entries;
+    dombft::ClientRecords clientRecords;
 };
 
 bool getLogSuffixFromProposal(const dombft::proto::FallbackProposal &fallbackProposal, LogSuffix &logSuffix);
-bool applySuffixToLog(const LogSuffix &logSuffix, std::shared_ptr<Log> log);
+bool applySuffixToLog(LogSuffix &logSuffix, const std::shared_ptr<Log>& log);
 
 #endif
