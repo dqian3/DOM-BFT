ede41c5ea2096d3dcda1265c9f52a5c8a5cc99a2
Hoooao <93057312+Hoooao@users.noreply.github.com>
2024-11-11 23:08:03 -0500

    Checkpoint refactor & overlapping (#75)
    * Refactor checkpoint components in replica
    
    * refactor
    
    * overlap fix init
    
    * fallback refactor
    
    * test
    
    fin

--- lib/client_record.h
+++ lib/client_record.h
@@ -5,25 +5,26 @@
 #include "lib/utils.h"
 #include "proto/dombft_proto.pb.h"
 #include <glog/logging.h>
-#include <iostream>
 
 namespace dombft {
 using namespace dombft::proto;
+class ClientRecord;
+typedef std::unordered_map<uint32_t, ClientRecord> ClientRecords;
 
 class ClientRecord {
 public:
     uint32_t instance_ = 0;
     uint32_t lastSeq_ = 0;
     std::unordered_set<uint32_t> missedSeqs_;
+
+    bool updateRecordWithSeq(uint32_t newSeq);
 };
-typedef std::unordered_map<uint32_t, ClientRecord> ClientRecords;
 
 void getClientRecordsFromProto(
-    const CheckpointClientRecordsSet &records, std::unordered_map<uint32_t, ClientRecord> &dst
+    const CheckpointClientRecordsSet &records, ClientRecords &dst
 );
-bool updateRecordWithSeq(ClientRecord &cliRecord, uint32_t newSeq);
 void getRecordsDigest(const std::unordered_map<uint32_t, ClientRecord> &records, byte *digest);
-int getRightShiftNumWithRecords(const ClientRecords &records1, const ClientRecords &records2);
+int getRightShiftNumWithRecords(const ClientRecords &checkpointRecords, const ClientRecords &replicaRecords);
 
 bool verifyRecordDigestFromProto(const CheckpointClientRecordsSet &recordsSet);
 void toProtoClientRecords(
