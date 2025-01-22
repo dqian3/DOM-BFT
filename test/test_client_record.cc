887427dfa50db19e54d638d383354834e7468dc1
Daniel Qian <daniel.qian@nyu.edu>
2025-01-22 16:40:02 -0500

    fix tests

--- test/test_client_record.cc
+++ test/test_client_record.cc
@@ -12,7 +12,7 @@ protected:
     void SetUp() override
     {
         for (uint32_t i = 0; i < 5; i++)
-            clientRecord.updateRecordWithSeq(i);
+            clientRecord.update(i);
     }
 };
 
@@ -38,11 +38,11 @@ protected:
 TEST_F(ClientRecordUtilTest, UpdateRecordWithHigherSeq)
 {
     uint32_t initialSeq = 5;
-    clientRecord.updateRecordWithSeq(initialSeq);
+    clientRecord.update(initialSeq);
     EXPECT_EQ(clientRecord.lastSeq_, initialSeq);
 
     uint32_t newSeq = 10;
-    EXPECT_TRUE(clientRecord.updateRecordWithSeq(newSeq));
+    EXPECT_TRUE(clientRecord.update(newSeq));
     EXPECT_EQ(clientRecord.lastSeq_, newSeq);
     // Check missed sequences
     EXPECT_EQ(clientRecord.missedSeqs_.size(), newSeq - initialSeq - 1);
@@ -56,24 +56,24 @@ TEST_F(ClientRecordUtilTest, UpdateRecordWithHigherSeq)
 
 TEST_F(ClientRecordUtilTest, UpdateRecordWithSameSeq)
 {
-    clientRecord.updateRecordWithSeq(5);
-    EXPECT_FALSE(clientRecord.updateRecordWithSeq(5));
+    clientRecord.update(5);
+    EXPECT_FALSE(clientRecord.update(5));
     EXPECT_EQ(clientRecord.lastSeq_, 5);
 }
 
 TEST_F(ClientRecordUtilTest, UpdateRecordWithOldSeq)
 {
-    clientRecord.updateRecordWithSeq(5);
-    EXPECT_FALSE(clientRecord.updateRecordWithSeq(4));
+    clientRecord.update(5);
+    EXPECT_FALSE(clientRecord.update(4));
 }
 
 TEST_F(ClientRecordUtilTest, UpdateRecordWithMissingSeq)
 {
-    clientRecord.updateRecordWithSeq(5);
-    clientRecord.updateRecordWithSeq(10);
+    clientRecord.update(5);
+    clientRecord.update(10);
     uint32_t missedSeq = 6;
     EXPECT_TRUE(clientRecord.missedSeqs_.contains(missedSeq));
-    EXPECT_TRUE(clientRecord.updateRecordWithSeq(missedSeq));
+    EXPECT_TRUE(clientRecord.update(missedSeq));
     EXPECT_FALSE(clientRecord.missedSeqs_.contains(missedSeq));
 }
 
@@ -83,8 +83,8 @@ TEST_F(ClientRecordShiftTest, GetShiftNumWhenSlow)
     // client 0 is slow on replica
     replicaRecords[0].lastSeq_ = 8;
     replicaRecords[0].missedSeqs_ = {6, 7};
-    replicaRecords[1].updateRecordWithSeq(19);
-    replicaRecords[1].updateRecordWithSeq(20);
+    replicaRecords[1].update(19);
+    replicaRecords[1].update(20);
 
     EXPECT_EQ(getRightShiftNumWithRecords(checkpointRecords, replicaRecords), 2);
 };
@@ -103,7 +103,7 @@ TEST_F(ClientRecordShiftTest, GetShiftNumWhenMissAndSlow)
     replicaRecords[0].lastSeq_ = 8;
     replicaRecords[0].missedSeqs_ = {5, 6};
 
-    replicaRecords[1].updateRecordWithSeq(19);
+    replicaRecords[1].update(19);
 
     EXPECT_EQ(getRightShiftNumWithRecords(checkpointRecords, replicaRecords), 2);
 };
