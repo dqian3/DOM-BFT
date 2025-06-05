f910414a9ff97dda7f91fedeaf47c8c05dc43c67
Jinkun <steam1994@163.com>
2025-06-05 14:12:27 +0000

    move verify macro inside rrr namespace -- resolve name conflict

--- lib/rrr/base/debugging.hpp
+++ lib/rrr/base/debugging.hpp
@@ -1,28 +1,35 @@
 #pragma once
 
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
 
 #ifndef likely
-#define likely(x)   __builtin_expect((x), 1)
-#endif // likely
+#define likely(x) __builtin_expect((x), 1)
+#endif   // likely
 
 #ifndef unlikely
-#define unlikely(x)   __builtin_expect((x), 0)
-#endif // unlikely
+#define unlikely(x) __builtin_expect((x), 0)
+#endif   // unlikely
+
+namespace rrr {
 
 /**
  * Use assert() when the test is only intended for debugging.
  * Use verify() when the test is crucial for both debug and release binary.
  */
 #ifdef NDEBUG
-#define verify(expr) do { if (unlikely(!(expr))) { printf("  *** verify failed: %s at %s, line %d\n", #expr, __FILE__, __LINE__); ::rrr::print_stack_trace(); ::abort(); } } while (0)
+#define verify(expr)                                                                                                   \
+    do {                                                                                                               \
+        if (unlikely(!(expr))) {                                                                                       \
+            printf("  *** verify failed: %s at %s, line %d\n", #expr, __FILE__, __LINE__);                             \
+            ::rrr::print_stack_trace();                                                                                \
+            ::abort();                                                                                                 \
+        }                                                                                                              \
+    } while (0)
 #else
 #define verify(expr) assert(expr)
 #endif
 
-namespace rrr {
+void print_stack_trace(FILE *fp = stderr) __attribute__((noinline));
 
-void print_stack_trace(FILE* fp = stderr) __attribute__((noinline));
-
-} // namespace base
+}   // namespace rrr
